cab057db3335c327a16649efa1a97d4bc7ee466d
Youngsoo, Na <pullinear@daum.net>
2020-09-08 19:30:00 +0900

    chater4 finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
 
-function App() {
+function Food({name, picture, rating} ) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h2>I love {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name}/>
     </div>
   );
 }
 
+const foodILike = [
+  {
+    id: '1',
+    name: 'kimchi',
+    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
+    rating : 5,
+  },
+  {
+    id: '2',
+    name: 'kimchi2',
+    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
+    rating : 4,
+  },
+  {
+    id: '3',
+    name: 'kimchi3',
+    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
+    rating : 5,
+  },
+  {
+    id: '4',
+    name: 'kimchi4',
+    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
+    rating : 3.5,
+  },
+  {
+    id: '5',
+    name: 'kimchi5',
+    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
+    rating : 3,
+  },
+];
+
+
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name:    PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating:  PropTypes.number,
+  // number인데 string으로 지정해놔서 경고가 뜸
+  // undefined 도 경고해줌
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />,document.getElementById('root'));
+// React가 document 의 id가 root인 곳에 <App />을 그린다
+// 인접한 JSX 요소는 반드시 하나의 태그로 감싸야 합니다. 
+// 무조건 App에다가 다 넣어야 하나? 
