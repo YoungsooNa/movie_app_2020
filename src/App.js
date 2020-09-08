80e691e54705835e8b1395a919a30683f850abc4
Youngsoo, Na <pullinear@daum.net>
2020-09-09 08:26:25 +0900

    05-3

--- src/App.js
+++ src/App.js
@@ -1,65 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React from 'react'
 
-function Food({name, picture, rating} ) {
-  return (
-    <div>
-    <h2>I love {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name}/>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('Hello');
+  }
+
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  }
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  }
+  componentDidMount() {
+    console.log("mounted");
+  }
+  componentDidUpdate() {
+    console.log("update");
+  }
+
+  componentWillUnmount() {
+    console.log("deleted");
+  }
+  render() {
+    console.log('redner');
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>PLUS</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    );
+  }
 }
-
-const foodILike = [
-  {
-    id: '1',
-    name: 'kimchi',
-    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
-    rating : 5,
-  },
-  {
-    id: '2',
-    name: 'kimchi2',
-    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
-    rating : 4,
-  },
-  {
-    id: '3',
-    name: 'kimchi3',
-    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
-    rating : 5,
-  },
-  {
-    id: '4',
-    name: 'kimchi4',
-    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
-    rating : 3.5,
-  },
-  {
-    id: '5',
-    name: 'kimchi5',
-    image: 'https://i2.wp.com/chopchopchoi.com/wp-content/uploads/2020/03/KimchiFriedRice-scaled.jpg?w=1024&ssl=1',
-    rating : 3,
-  },
-];
-
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name:    PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating:  PropTypes.number,
-  // number인데 string으로 지정해놔서 경고가 뜸
-  // undefined 도 경고해줌
-};
 export default App;
